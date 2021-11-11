969e8c98d5d87375831d68cfefacf7aef4d1a691
Manuel Olguín Muñoz <manuel@olguin.se>
2021-11-11 15:39:07 -0500

    easy conversion to json

--- ainur/hosts.py
+++ ainur/hosts.py
@@ -1,22 +1,37 @@
 from __future__ import annotations
 
-import re
-from dataclasses import dataclass
-from ipaddress import IPv4Interface
+from dataclasses import Field, dataclass, field
+from ipaddress import IPv4Address, IPv4Interface
+from typing import Type
+
+from dataclasses_json import config, dataclass_json
 
 
+def ip_field(cls: Type[IPv4Address], *args, **kwargs) -> Field:
+    return field(
+        *args,
+        **kwargs,
+        metadata=config(
+            encoder=str,
+            decoder=cls
+        )
+    )
+
+
+@dataclass_json
 @dataclass(frozen=True, eq=True)
 class AnsibleHost:
-    name: str
     ansible_host: str
 
 
+@dataclass_json
 @dataclass(frozen=True, eq=True)
 class DisconnectedWorkloadHost(AnsibleHost):
-    management_ip: IPv4Interface
     workload_nic: str
+    management_ip: IPv4Interface = ip_field(IPv4Interface)
 
 
+@dataclass_json
 @dataclass(frozen=True, eq=True)
 class ConnectedWorkloadHost(DisconnectedWorkloadHost):
-    workload_ip: IPv4Interface
+    workload_ip: IPv4Interface = ip_field(IPv4Interface)
