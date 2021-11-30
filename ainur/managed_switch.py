03265db4c9013065baa3ce27005d0b3d130f06dc
Seyed Samie Mostafavi <ssmos@kth.se>
2021-11-30 13:13:18 +0000

    new managed_switch, sdr_manager, and tc files

---
+++ ainur/managed_switch.py
@@ -0,0 +1,304 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+from collections import defaultdict
+
+from contextlib import AbstractContextManager
+from ipaddress import IPv4Interface, IPv4Network
+from typing import FrozenSet, Mapping
+from operator import attrgetter
+
+from .hosts import WiFi,Wire
+
+import ansible_runner
+from loguru import logger
+
+import pexpect
+
+@dataclass(frozen=True, eq=True)
+class Vlan():
+    default: bool
+    name: str
+    id_num: int
+    switch_name: str
+    ports: list[int]
+
+class ManagedSwitch(AbstractContextManager):
+    """
+    Represents a managed network switch.
+
+    Can be used as a context manager for easy vlan deployment on a managed switch 
+    and automatic teardown of vlans.
+    """
+
+    def __init__(self,
+                 name: str,
+                 credentials: Tuple[str,str],
+                 address: IPv4Interface,
+                 timeout: int,
+                 quiet: bool = True):
+
+        # we do login - logout for every command since there is a timeout for
+        # each login session on the cisco switch
+
+        self._name = name
+        self._address = address
+        self._timeout = timeout
+        self._quiet = quiet
+
+        #Second argument is assigned to the variable user
+        self._user = credentials[0]
+        #Third argument is assigned to the variable password
+        self._password = credentials[1]
+
+        logger.info('Contacting the network switch.')
+
+        # update vlans table
+        self.update_vlans()
+
+
+    @property
+    def vlans(self) -> FrozenSet[Vlan]:
+        return frozenset(self._vlans)
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def credentials(self) -> Tuple[str,str]:
+        return self._credentials
+
+    @property
+    def address(self) -> IPv4Network:
+        return self._address
+    
+    @property
+    def timeout(self) -> int:
+        return self._timeout
+
+    def login(self):
+
+        #This spawns the telnet program and connects it to the variable name
+        child = pexpect.spawn("telnet %s" % self._name, timeout=self._timeout)
+
+        #The script expects login
+        child.expect_exact("Username:")
+        child.send(self._user+"\n")
+
+        #The script expects Password
+        child.expect_exact("Password:")
+        child.send(self._password+"\n")
+
+        child.expect_exact(self._name+"#")
+
+        # telnet connection is ready
+        return child
+
+    def logout(self,child):
+
+        # assume we are in login state
+        child.send("exit\n")
+        child.send("exit\n")
+        child.expect(pexpect.EOF)
+
+    def make_connections(self, inventory, conn_specs):
+
+        workload_hosts = inventory['hosts']
+        radios = inventory['radios']
+
+        # Make workload switch vlans
+        ports_wirednets = {}
+        for host_name in conn_specs.keys():
+            for if_name in conn_specs[host_name].keys():
+                phy = conn_specs[host_name][if_name].phy
+                interface = workload_hosts[host_name].interfaces[if_name]
+                if isinstance(phy,WiFi):
+                    # WiFi sdr vlans
+                    if phy.radio != 'native':
+                        host_port = interface.switch_connection.port
+                        ports = [host_port,radios[phy.radio].switch_connection.port]
+                        vlan_name = workload_hosts[host_name].ansible_host+'_to_'+phy.radio
+                        self.make_vlan(ports=ports,name=vlan_name)
+                elif isinstance(phy,Wire):
+                    # wired nodes vlans
+                    # connect the ones with the same network name
+                    ports_wirednets[interface.switch_connection.port] = phy.network
+
+        # Grouping dictionary by values and make lists of ports for each network name
+        wired_nets = defaultdict(list)
+        for key, value in sorted(ports_wirednets.items()):
+            wired_nets[value].append(key)
+
+        for wired_net_name, ports_list in wired_nets.items():
+            self.make_vlan(ports=ports_list,name=wired_net_name)
+
+
+    def update_vlans(self):
+
+        child = self.login()
+
+        vlans = []
+        child.send("show vlan\n")
+
+        # go to config mode (necessary for having one line after the table)
+        child.send("configure terminal\n")
+        child.expect_exact(self._name+'(config)#')
+
+        lines = child.before.splitlines()
+
+        del lines[0]  # delete first 4 lines
+        del lines[-2:]  # delete last 4 lines
+
+        #log
+        if not self._quiet:
+            for item in lines:
+                print(item.decode("utf-8"))
+
+        del lines[0:3]  # delete first 4 lines
+
+        # log
+        if not self._quiet:
+            print('Number of vlans: %d' % len(lines))
+
+        for idx, line in enumerate(lines):
+            line = line.decode('utf-8')
+            result = [x.strip() for x in line.split('|')]
+            vlan_id = int(result[0])
+            vlan_name = result[1]
+            ports_str = re.sub("[^0-9-]", " ", result[2])
+            vlan_ports = []
+            for vlp in ports_str.split():
+                if '-' not in vlp:
+                    vlan_ports.append(int(vlp))
+                else:
+                    nums = vlp.split('-')
+                    vlan_ports.extend([n for n in range(int(nums[0]),int(nums[1])+1)])
+
+            vlans.append(Vlan(name=vlan_name,id_num=vlan_id,ports=vlan_ports,switch_name=self._name, default=True))
+            if not self._quiet:
+                print('Vlan #%d id: %d, name: %s, ports: %s' % (idx,vlan_id,vlan_name,vlan_ports))#If it all goes pear shaped the script will timeout after 20 seconds.
+        
+        self._vlans = vlans
+        
+        # go back to login mode
+        child.send("exit\n")
+        child.expect_exact(self._name+"#")
+
+        self.logout(child)
+        
+        logger.info('Default vlans loaded.')
+
+
+    def make_vlan(self, ports: List[int], name: str) -> Vlan:
+
+        # to get the object with the characteristic (max id)
+        biggest_vlan_id = max(self._vlans, key=attrgetter("id_num"))
+        vlanid = biggest_vlan_id.id_num+1
+
+        child = self.login()
+        
+        # go to config mode
+        child.send("configure terminal\n")
+        child.expect_exact(self._name+'(config)#')
+
+        child.send("vlan %d\n" % vlanid)
+
+        child.expect_exact(self._name+"(config-vlan)#")
+        child.send("name %s\n" % name)
+
+        child.expect_exact(self._name+"(config-vlan)#")
+        child.send("exit\n")
+
+        for portnum in ports:
+
+            child.expect_exact(self._name+"(config)#")
+            child.send("interface gi%d\n" % portnum)
+
+            child.expect_exact(self._name+"(config-if)")
+            child.send("switchport mode access\n")
+
+            child.expect_exact(self._name+"(config-if)")
+            child.send("switchport access vlan %d\n" % vlanid)
+
+            child.expect_exact(self._name+"(config-if)")
+            child.send("exit\n")
+            
+        child.expect_exact(self._name+'(config)#')
+
+        # go back to login mode
+        child.send("exit\n")
+        child.expect_exact(self._name+"#")
+
+        new_vlan = Vlan(name=name,id_num=vlanid,ports=ports,switch_name=self._name, default=False)
+        self._vlans.append(new_vlan)
+        logger.info('New vlan with id: %d added.'% vlanid)
+        return new_vlan
+    
+    def hard_remove_vlan(self,id_num: int):
+
+        child = self.login()
+
+        # go to config mode
+        child.send("configure terminal\n")
+        child.expect_exact(self._name+'(config)#')
+
+        # remove it
+        child.send("no vlan %d\n" % id_num)
+        child.expect_exact(self._name+"(config)")
+
+        # go back to login mode
+        child.send("exit\n")
+        child.expect_exact(self._name+"#")
+
+        self.logout(child)
+
+        logger.warning('Workload switch vlan with id: %d is removed.' % id_num)
+
+    def remove_vlan(self, id_num: int):
+        vlan = [ vl for vl in self._vlans if vl.id_num == id_num ]
+        if len(vlan) == 0:
+            logger.error('There is no vlan with id: %d to be removed.' % id_num)
+            return
+        vlan = vlan[0]
+
+        child = self.login()
+        
+        # go to config mode
+        child.send("configure terminal\n")
+        child.expect_exact(self._name+'(config)#')
+
+        # remove it
+        child.send("no vlan %d\n" % id_num)
+        child.expect_exact(self._name+"(config)")
+
+        # go back to login mode
+        child.send("exit\n")
+        child.expect_exact(self._name+"#")
+
+        self._vlans.remove(vlan)
+        logger.warning('Workload switch vlan with id: %d is removed.' % id_num)
+        
+
+    def tear_down(self) -> None:
+        """
+        Removes all the added vlans.
+        Note that after calling this method, this object will be left in an
+        invalid state and should not be used any more.
+        """
+        logger.warning('Removing workload switch vlans.')
+
+        non_defalut_vlan_ids = [ vl.id_num for vl in self._vlans if vl.default == False]
+        # remove all non default vlans
+        for nd_id_num in non_defalut_vlan_ids:
+            self.remove_vlan(nd_id_num)
+
+        logger.warning('Workload switch non default vlans removed.')
+
+    def __enter__(self) -> ManagedSwitch:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.tear_down()
+        return super(ManagedSwitch, self).__exit__(exc_type, exc_val, exc_tb)
---
+++ ainur/sdr_manager.py
@@ -0,0 +1,207 @@
+from __future__ import annotations
+
+import docker
+from docker.utils import create_host_config
+
+from contextlib import AbstractContextManager
+from ipaddress import IPv4Interface, IPv4Network
+from typing import FrozenSet, Mapping
+import json
+import time
+
+from loguru import logger
+import socket
+import sys
+
+from .hosts import SoftwareDefinedRadio, WiFi
+
+
+#DOCKER_BASE_URL='unix://var/run/docker.sock'
+BEACON_INTERVAL = 100
+
+class SDRManager(AbstractContextManager):
+    """
+    Represents a network of SDRs.
+
+    Can be used as a context manager for easy sdr config container deployment
+    and automatic teardown of it.
+    """
+
+    def __init__(self,
+            sdrs: list[SoftwareDefinedRadio],
+            docker_base_url: str,
+            container_image_name: str,
+            sdr_config_addr: str,
+            use_jumbo_frames: bool = False,
+            quiet: bool = True):
+    
+
+        self._sdrs = sdrs
+        self._docker_base_url = docker_base_url
+        self._container_image_name = container_image_name
+        self._sdr_config_addr = sdr_config_addr
+        self._use_jumbo_frames = use_jumbo_frames
+
+
+        self._client = docker.APIClient(base_url=self._docker_base_url)
+        volumes= [self._sdr_config_addr]
+        volume_bindings = {
+                            self._sdr_config_addr: {
+                                'bind': '/home/host',
+                                'mode': 'rw',
+                            },
+        }
+
+        host_config = self._client.create_host_config(
+                            binds=volume_bindings,
+                            network_mode='host',
+        )
+        
+        self._container = self._client.create_container(
+                            image=self._container_image_name,
+                            volumes=volumes,
+                            host_config=host_config,
+        ) 
+        # start the container
+        self._client.start(self._container)
+        logger.info('sdr network container started.')
+
+        time.sleep(1)
+
+        # connect to the container server app
+        # start the socket and connect
+        HOST, PORT = "localhost", 50505
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.settimeout(5)
+        self._socket.connect((HOST, PORT))
+        
+        logger.info('socket connection to SDR network manager established.')
+
+        # send nodes_ini 
+        nodes_ini = {}
+        for name,sdr in sdrs.items():
+            nodes_ini[sdr.name] = { 'ip_address' : str(sdr.management_ip).split("/")[0] }
+
+        init_dict = {
+                        'nodes_ini' : nodes_ini,
+                        'use_jumbo_frames' : self._use_jumbo_frames,
+                        'management_network' : str(sdr.management_ip.network).split("/")[0]
+                    }
+        self.send_command('init',init_dict)
+        logger.info('SDR network manager container is up.')
+
+    def start_network(self,
+            ap_wifi: WiFi,
+            sta_wifis : List[WiFi],
+            foreign_sta_macs : List[str],
+            wifi_network: WiFiNetwork,
+        ):
+
+        logger.info(f'Starting SDR network with ssid: {wifi_network.ssid} on access point {ap_wifi.radio}.')
+        # make start network command dict
+        sta_sdr_names_dict = {}
+        for idx,sta_wifi in enumerate(sta_wifis):
+            sta_sdr_names_dict['name_'+str(idx+1)] = sta_wifi.radio
+        if len(sta_wifis) == 0:
+            sta_sdr_names_dict = ''
+
+        foreign_sta_macs_dict = {}
+        for idx,mac in enumerate(foreign_sta_macs):
+            foreign_sta_macs_dict['mac_'+str(idx+1)] = mac
+        if len(foreign_sta_macs) == 0:
+            foreign_sta_macs_dict = ''
+
+        start_sdrs_cmd = {
+            'general' : { 
+                'ssid' : wifi_network.ssid,
+                'channel' : str(wifi_network.channel),
+                'beacon_interval' : wifi_network.beacon_interval,
+                'ht_capable' : wifi_network.ht_capable,
+                'ap_sdr_name' : ap_wifi.radio,
+            },
+            'sta_sdr_names' : sta_sdr_names_dict,
+            'foreign_sta_macs' : foreign_sta_macs_dict,
+        }
+       
+        self.send_command('start',start_sdrs_cmd)
+        logger.info(f'SDR network with ssid: {wifi_network.ssid} is up.')
+
+    def send_command(self,
+            command_type: str,
+            content: dict):
+
+        cmd = {
+            'command' : command_type,
+            'content' : content,
+        }
+
+        msg = str(json.dumps(cmd))
+        self._socket.sendall(bytes(msg + "\n", "utf-8"))
+
+        # Receive response from the server
+        received = str(self._socket.recv(3072), "utf-8")
+        result_dict = json.loads(received)
+        if result_dict['outcome'] == 'failed':
+            logger.error(result_dict['content']['msg'])
+
+    def find_stations_of_ap(self,workload_hosts,conn_specs,wifi_sdr_ap):
+        # find the wifi stations meant to be connected to the wifi_sdr_ap
+        sdr_stations = []
+        native_stations_macs = []
+        for host_name_ in conn_specs.keys():
+            for if_name_ in conn_specs[host_name_].keys():
+                phy_ = conn_specs[host_name_][if_name_].phy
+                if isinstance(phy_,WiFi):
+                    if (phy_.radio != 'native') and (not phy_.is_ap):
+                        if phy_.network == wifi_sdr_ap.network :
+                            sdr_stations.append(phy_)
+                elif isinstance(phy_,WiFi):
+                    if (phy_.radio == 'native') and (not phy_.is_ap):
+                        if phy_.network == wifi_sdr_ap.network :
+                            native_phy_mac = workload_hosts[host_name_].interfaces[if_name_].mac
+                            native_stations_macs.append(native_phy_mac)
+
+        return sdr_stations,native_stations_macs
+
+    def create_wlans(self,workload_hosts,conn_specs,networks):
+        # Setup up the SDR network
+        # find wlan_aps and create a wlan network per SDR AP
+        # find sdr station wifis and foreign_sta_macs
+        for host_name in conn_specs.keys():
+            for if_name in conn_specs[host_name].keys():
+                phy = conn_specs[host_name][if_name].phy
+                # create a wlan network per SDR AP
+                if isinstance(phy,WiFi):
+                    if (phy.radio != 'native') and (phy.is_ap):
+                        wifi_sdr_ap = phy
+                        sdr_stations,native_stations_macs = self.find_stations_of_ap(workload_hosts=workload_hosts,conn_specs=conn_specs,wifi_sdr_ap=wifi_sdr_ap)
+                        # start a network
+                        self.start_network(
+                            ap_wifi = wifi_sdr_ap,
+                            sta_wifis = sdr_stations,
+                            foreign_sta_macs = native_stations_macs,
+                            wifi_network = networks[phy.network],
+                        )
+
+    def tear_down(self) -> None:
+        """
+        Stop the SDR network config container.
+        Note that after calling this method, this object will be left in an
+        invalid state and should not be used any more.
+        """
+        logger.warning('Tearing down SDR network.')
+
+        self.send_command('tear_down','')
+        # close the socker
+        self._socket.close()
+        # stop the container
+        self._client.stop(container=self._container, timeout=1)
+
+        logger.warning('SDR network is stopped.')
+
+    def __enter__(self) -> SDRManager:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.tear_down()
+        return super(SDRManager, self).__exit__(exc_type, exc_val, exc_tb)
