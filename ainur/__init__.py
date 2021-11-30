c523ca5da4112fe96dc14164ad0873f4452cc38f
Seyed Samie Mostafavi <ssmos@kth.se>
2021-11-30 13:31:17 +0000

    new __init__.py file

--- ainur/__init__.py
+++ ainur/__init__.py
@@ -1,11 +1,26 @@
 from .ansible import AnsibleContext
-from .network import NetworkLayer
+from .network import WorkloadNetwork
+from .physical import PhysicalLayer
 from .swarm import DockerSwarm
-from .hosts import WorkloadHost, Layer3ConnectedWorkloadHost, AnsibleHost
+from .managed_switch import ManagedSwitch
+from .sdr_manager import SDRManager
+from .tc import TrafficControl
+from .hosts import WorkloadHost,AnsibleHost,ConnectedWorkloadHost
+from .hosts import NetplanInterface, EthernetInterface, WiFiInterface
+from .hosts import WiFi, Phy, Wire
+from .hosts import Switch, SwitchConnection, SoftwareDefinedRadio, PhyNetwork, WiFiNetwork, WiredNetwork, ConnectionSpec
 
 __all__ = [
     'AnsibleContext',
-    'NetworkLayer',
+    'WorkloadNetwork',
+    'PhysicalLayer',
     'DockerSwarm',
-    'WorkloadHost', 'Layer3ConnectedWorkloadHost', 'AnsibleHost'
+    'WorkloadHost','AnsibleHost','ConnectedWorkloadHost',
+    'NetplanInterface', 'ConnectionSpec','EthernetInterface','WiFiInterface',
+    'Phy','WiFi','Wire',
+    'SwitchConnection','SoftwareDefinedRadio',
+    'PhyNetwork','WiFiNetwork','WiredNetwork',
+    'ManagedSwitch','Switch',
+    'TrafficControl',
+    'SDRManager'
 ]
--- phy_test.py
+++ phy_test.py
@@ -1,111 +1,112 @@
 from ipaddress import IPv4Interface
 from pathlib import Path
-import json
-import ansible_runner
-
 
 from ainur import *
-
 
 if __name__ == '__main__':
     # quick test to verify network + swarm work
 
-
-    inventory = { 
+    inventory = {
         'hosts' : {
-            'workload-client-04' : WorkloadHost(
-                    ansible_host='workload-client-04',
-                    management_ip=IPv4Interface('192.168.1.104/24'),
-                    interfaces = { 'eth0': EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:bf:53:b8',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=29),
-                                            ),
-                    },
+            'workload-client-04': WorkloadHost(
+                ansible_host='workload-client-04',
+                management_ip=IPv4Interface('192.168.1.104/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:bf:53:b8',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=29),
+                ),
+                },
             ),
-            'workload-client-05' : WorkloadHost(
-                    ansible_host='workload-client-05',
-                    management_ip=IPv4Interface('192.168.1.105/24'),
-                    interfaces = { 'eth0': EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:07:fe:f2',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=30),
-                                            ),
-                    },
+            'workload-client-05': WorkloadHost(
+                ansible_host='workload-client-05',
+                management_ip=IPv4Interface('192.168.1.105/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:07:fe:f2',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=30),
+                ),
+                },
             ),
-            'workload-client-06' : WorkloadHost(
-                    ansible_host='workload-client-06',
-                    management_ip=IPv4Interface('192.168.1.106/24'),
-                    interfaces = { 'eth0' : EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:bf:53:f4',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=31),
-                                            ),
-                    },
+            'workload-client-06': WorkloadHost(
+                ansible_host='workload-client-06',
+                management_ip=IPv4Interface('192.168.1.106/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:bf:53:f4',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=31),
+                ),
+                },
             ),
-            'workload-client-07' : WorkloadHost(
-                    ansible_host='workload-client-07',
-                    management_ip=IPv4Interface('192.168.1.107/24'),
-                    interfaces = { 'eth0' : EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:bf:52:83',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=32),
-                                            ),
-                    },
-            ), 
-            'workload-client-08' : WorkloadHost(
-                    ansible_host='workload-client-08',
-                    management_ip=IPv4Interface('192.168.1.108/24'),
-                    interfaces = { 'eth0' : EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:bf:54:12',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=33),
-                                            ),
-                                            'wlan0' : WiFiInterface(
-                                                netplan_type='wifis',
-                                                name='wlan0',
-                                                mac='f0:2f:74:63:5c:d9',
-                                            ),
-                    },
+            'workload-client-07': WorkloadHost(
+                ansible_host='workload-client-07',
+                management_ip=IPv4Interface('192.168.1.107/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:bf:52:83',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=32),
+                ),
+                },
             ),
-            'workload-client-09' : WorkloadHost(
-                    ansible_host='workload-client-09',
-                    management_ip=IPv4Interface('192.168.1.109/24'),
-                    interfaces = { 'eth0': EthernetInterface(
-                                                netplan_type='ethernets',
-                                                name='eth0',
-                                                mac='dc:a6:32:bf:53:40',
-                                                switch_connection=SwitchConnection(name='glorfindel',port=34),
-                                            ),
-                    }, 
+            'workload-client-08': WorkloadHost(
+                ansible_host='workload-client-08',
+                management_ip=IPv4Interface('192.168.1.108/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:bf:54:12',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=33),
+                ),
+                    'wlan0'       : WiFiInterface(
+                        netplan_type='wifis',
+                        name='wlan0',
+                        mac='f0:2f:74:63:5c:d9',
+                    ),
+                },
+            ),
+            'workload-client-09': WorkloadHost(
+                ansible_host='workload-client-09',
+                management_ip=IPv4Interface('192.168.1.109/24'),
+                interfaces={'eth0': EthernetInterface(
+                    netplan_type='ethernets',
+                    name='eth0',
+                    mac='dc:a6:32:bf:53:40',
+                    switch_connection=SwitchConnection(name='glorfindel',
+                                                       port=34),
+                ),
+                },
             ),
         },
-        'radios' : {
+        'radios': {
             'RFSOM-00001': SoftwareDefinedRadio(
                 name='RFSOM-00001',
                 mac='02:05:f7:80:0b:72',
                 management_ip=IPv4Interface('192.168.1.61/24'),
-                switch_connection=SwitchConnection(name='glorfindel',port=41)
+                switch_connection=SwitchConnection(name='glorfindel', port=41)
             ),
             'RFSOM-00002': SoftwareDefinedRadio(
                 name='RFSOM-00002',
                 mac='02:05:f7:80:0b:19',
                 management_ip=IPv4Interface('192.168.1.62/24'),
-                switch_connection=SwitchConnection(name='glorfindel',port=42)
+                switch_connection=SwitchConnection(name='glorfindel', port=42)
             ),
             'RFSOM-00003': SoftwareDefinedRadio(
                 name='RFSOM-00003',
                 mac='02:05:f7:80:02:c8',
                 management_ip=IPv4Interface('192.168.1.63/24'),
-                switch_connection=SwitchConnection(name='glorfindel',port=43)
+                switch_connection=SwitchConnection(name='glorfindel', port=43)
             ),
-        }, 
-        'switch' : Switch(
+        },
+        'switch': Switch(
             name='glorfindel',
             management_ip='192.168.1.5/24',
             username='cisco',
@@ -116,48 +117,48 @@ if __name__ == '__main__':
     # build up the workload networks
     # channels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48]
     workload_network_desc = {
-        'subnetworks' : {
-            'wlan_net' : WiFiNetwork(
-                name = 'wlan_net',
-                ssid = 'expeca_wlan_1',
-                channel = 11,
-                beacon_interval = 100,
-                ht_capable = True,
+        'subnetworks'     : {
+            'wlan_net': WiFiNetwork(
+                name='wlan_net',
+                ssid='expeca_wlan_1',
+                channel=11,
+                beacon_interval=100,
+                ht_capable=True,
             ),
             'eth_net' : WiredNetwork(
-                name = 'eth_net',
+                name='eth_net',
             ),
         },
-        'connection_specs' : {
-            'workload-client-07' : { 'eth0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.0.7/24'), 
-                    phy=WiFi(network='wlan_net', radio='RFSOM-00002', is_ap=True),
-                ),
+        'connection_specs': {
+            'workload-client-07': {'eth0': ConnectionSpec(
+                ip=IPv4Interface('10.0.0.7/24'),
+                phy=WiFi(network='wlan_net', radio='RFSOM-00002', is_ap=True),
+            ),
             },
-            'workload-client-08' : { 'wlan0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.0.8/24'),
-                    phy=WiFi(network='wlan_net', radio='native', is_ap=False),
-                ),
-            }, 
-            'workload-client-09' : { 'eth0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.0.9/24'),
-                    phy=WiFi(network='wlan_net', radio='RFSOM-00001', is_ap=False),
-                ),
-            }, 
-            'workload-client-04' : { 'eth0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.1.4/24'),
-                    phy=Wire(network='eth_net'),
-                ),
-            }, 
-            'workload-client-05' : { 'eth0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.1.5/24'),
-                    phy=Wire(network='eth_net'),
-                ),
-            }, 
-            'workload-client-06' : { 'eth0' : ConnectionSpec( 
-                    ip=IPv4Interface('10.0.1.6/24'),
-                    phy=Wire(network='eth_net'),
-                ),
+            'workload-client-08': {'wlan0': ConnectionSpec(
+                ip=IPv4Interface('10.0.0.8/24'),
+                phy=WiFi(network='wlan_net', radio='native', is_ap=False),
+            ),
+            },
+            'workload-client-09': {'eth0': ConnectionSpec(
+                ip=IPv4Interface('10.0.0.9/24'),
+                phy=WiFi(network='wlan_net', radio='RFSOM-00001', is_ap=False),
+            ),
+            },
+            'workload-client-04': {'eth0': ConnectionSpec(
+                ip=IPv4Interface('10.0.1.4/24'),
+                phy=Wire(network='eth_net'),
+            ),
+            },
+            'workload-client-05': {'eth0': ConnectionSpec(
+                ip=IPv4Interface('10.0.1.5/24'),
+                phy=Wire(network='eth_net'),
+            ),
+            },
+            'workload-client-06': {'eth0': ConnectionSpec(
+                ip=IPv4Interface('10.0.1.6/24'),
+                phy=Wire(network='eth_net'),
+            ),
             },
         }
     }
@@ -165,6 +166,6 @@ if __name__ == '__main__':
     ansible_ctx = AnsibleContext(base_dir=Path('./ansible_env'))
 
     # Start phy layer
-    with PhysicalLayer(inventory, workload_network_desc, ansible_ctx, ansible_quiet=True) as phy_layer:
+    with PhysicalLayer(inventory, workload_network_desc, ansible_ctx,
+                       ansible_quiet=True) as phy_layer:
         input("Press Enter to continue...")
-        
