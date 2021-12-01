78c5e2e87b425a9bf7660b2c2849d0ed97449442
Manuel Olguín Muñoz <manuel@olguin.se>
2021-12-01 11:01:58 +0100

    review init

--- ainur/__init__.py
+++ ainur/__init__.py
@@ -1,26 +1,23 @@
 from .ansible import AnsibleContext
+from .hosts import *
+from .managed_switch import ManagedSwitch
 from .network import NetworkLayer
 from .physical import PhysicalLayer
-from .swarm import DockerSwarm
-from .managed_switch import ManagedSwitch
 from .sdr_manager import SDRManager
+from .swarm import DockerSwarm
 from .tc import TrafficControl
-from .hosts import WorkloadHost,AnsibleHost,ConnectedWorkloadHost
-from .hosts import NetplanInterface, EthernetInterface, WiFiInterface
-from .hosts import WiFi, Phy, Wire
-from .hosts import Switch, SwitchConnection, SoftwareDefinedRadio, PhyNetwork, WiFiNetwork, WiredNetwork, ConnectionSpec
 
 __all__ = [
     'AnsibleContext',
     'NetworkLayer',
     'PhysicalLayer',
     'DockerSwarm',
-    'WorkloadHost','AnsibleHost','ConnectedWorkloadHost',
-    'NetplanInterface', 'ConnectionSpec','EthernetInterface','WiFiInterface',
-    'Phy','WiFi','Wire',
-    'SwitchConnection','SoftwareDefinedRadio',
-    'PhyNetwork','WiFiNetwork','WiredNetwork',
-    'ManagedSwitch','Switch',
+    'WorkloadHost', 'AnsibleHost', 'ConnectedWorkloadHost',
+    'NetplanInterface', 'ConnectionSpec', 'EthernetInterface', 'WiFiInterface',
+    'Phy', 'WiFi', 'Wire',
+    'SwitchConnection', 'SoftwareDefinedRadio',
+    'PhyNetwork', 'WiFiNetwork', 'WiredNetwork',
+    'ManagedSwitch', 'Switch',
     'TrafficControl',
     'SDRManager'
 ]
