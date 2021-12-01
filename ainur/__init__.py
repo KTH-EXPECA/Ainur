d0daf4f9fd3383e96fd815219a98e5c0fc9e677a
Manuel Olguín Muñoz <manuel@olguin.se>
2021-12-01 13:49:58 +0100

    iport .swarm

--- ainur/__init__.py
+++ ainur/__init__.py
@@ -5,7 +5,7 @@ from .network import NetworkLayer
 from .physical import PhysicalLayer
 from .sdr_manager import SDRManager
 from .tc import TrafficControl
-from swarm import *
+from .swarm import *
 
 __all__ = [
     'AnsibleContext',
