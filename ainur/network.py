db77c05fced1efdeb7fe9a2858b0be808eb2305a
Manuel Olguín Muñoz <manuel@olguin.se>
2021-12-08 14:01:05 +0100

    cloud

--- ainur/network.py
+++ ainur/network.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import abc
 import functools
 import json
 from contextlib import AbstractContextManager
 from ipaddress import IPv4Interface, IPv4Network
-from typing import Any, Iterator, Mapping
+from typing import Any, Collection, Iterator, Mapping
 
 import ansible_runner
 from frozendict import frozendict
@@ -21,8 +22,54 @@ class Layer3Error(Exception):
     pass
 
 
-class NetworkLayer(AbstractContextManager,
-                   Mapping[str, Layer3ConnectedWorkloadHost]):
+class Layer3Network(AbstractContextManager,
+                    Mapping[str, Layer3ConnectedWorkloadHost]):
+    @abc.abstractmethod
+    def __iter__(self) -> Iterator[str]:
+        pass
+
+    @abc.abstractmethod
+    def __getitem__(self, item: str) -> Layer3ConnectedWorkloadHost:
+        pass
+
+    @abc.abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def __contains__(self, item: Any) -> bool:
+        pass
+
+    @abc.abstractmethod
+    @property
+    def is_down(self) -> bool:
+        pass
+
+    @abc.abstractmethod
+    @property
+    def hosts(self) -> frozendict[str, Layer3ConnectedWorkloadHost]:
+        pass
+
+    @abc.abstractmethod
+    @property
+    def address(self) -> IPv4Network:
+        pass
+
+    @abc.abstractmethod
+    def tear_down(self) -> None:
+        pass
+
+    def __enter__(self) -> Layer3Network:
+        if self.is_down:
+            raise RuntimeError('Network has already been torn down.')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.tear_down()
+        return super(Layer3Network, self).__exit__(exc_type, exc_val, exc_tb)
+
+
+class NetworkLayer(Layer3Network):
     """
     Represents a connected workload network.
 
@@ -167,14 +214,130 @@ class NetworkLayer(AbstractContextManager,
         self._torn_down = True
         logger.warning('Workload network has been torn down.')
 
-    def __enter__(self) -> NetworkLayer:
-        if self._torn_down:
-            raise RuntimeError('Network has already been torn down.')
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
-        self.tear_down()
-        return super(NetworkLayer, self).__exit__(exc_type, exc_val,
-                                                  exc_tb)
 
 # TODO: need a way to test network locally
+
+
+class VPNRouting(AbstractContextManager):
+    def __init__(self,
+                 base_network: NetworkLayer,
+                 gateway_host: str,
+                 remote_networks: Collection[IPv4Network],
+                 ansible_ctx: AnsibleContext,
+                 ansible_quiet: bool = True):
+        # assumes VPN is set up
+
+        self._network = base_network
+        self._gw_host = gateway_host
+        self._remote_nets = remote_networks
+        self._ansible_ctx = ansible_ctx
+        self._ansible_quiet = ansible_quiet
+
+        # build the inventory
+        workload_gw_ip = self._network[self._gw_host].workload_ip.ip
+        management_gw_ip = self._network[self._gw_host].management_ip.ip
+
+        self._inventory = {
+            'all': {
+                'children': {
+                    'workload': {
+                        'vars' : {
+                            'gateway'    : str(workload_gw_ip),
+                            'remote_nets': [str(net) for net in remote_networks]
+                        },
+                        'hosts': {
+                            name: host.to_dict()
+                            for name, host in self._network.items()
+                            if name != self._gw_host
+                        }
+                    },
+                    'management': {
+                        'vars' : {
+                            'gateway'    : str(management_gw_ip),
+                            'remote_nets': [str(net) for net in remote_networks]
+                        },
+                        'hosts': {
+                            'localhost': {
+                                'ansible_connection': 'local'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        logger.debug(
+            f'Inventory for routing setup:\n{self._inventory}'
+        )
+
+    def __enter__(self) -> VPNRouting:
+        logger.info(f'Setting up routing to the cloud through {self._gw_host}.')
+        with self._ansible_ctx(self._inventory) as tmp_dir:
+            res = ansible_runner.run(
+                playbook='routing_up.yml',
+                json_mode=True,
+                private_data_dir=str(tmp_dir),
+                quiet=self._ansible_quiet,
+            )
+
+            # TODO: better error checking
+            assert res.status != 'failed'
+            # routing is up
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        logger.info('Tearing down routing to the cloud.')
+        with self._ansible_ctx(self._inventory) as tmp_dir:
+            res = ansible_runner.run(
+                playbook='routing_down.yml',
+                json_mode=True,
+                private_data_dir=str(tmp_dir),
+                quiet=self._ansible_quiet,
+            )
+
+            # TODO: better error checking
+            assert res.status != 'failed'
+            # routing is down
+
+
+class CloudNetworkLayer(Layer3Network):
+    def __init__(self,
+                 network: Layer3Network,
+                 cloud_hosts: Mapping[str, Layer3ConnectedWorkloadHost]):
+        self._local_net = network
+        self._cloud_hosts = frozendict(cloud_hosts)
+
+    def __iter__(self) -> Iterator[str]:
+        hosts = dict(self._local_net)
+        hosts.update(self._cloud_hosts)
+        return iter(hosts)
+
+    def __getitem__(self, item: str) -> Layer3ConnectedWorkloadHost:
+        try:
+            return self._local_net[item]
+        except KeyError:
+            return self._cloud_hosts[item]
+
+    def __len__(self) -> int:
+        return len(self._local_net) + len(self._cloud_hosts)
+
+    def __contains__(self, item: Any) -> bool:
+        return item in self._local_net or item in self._cloud_hosts
+
+    @property
+    def is_down(self) -> bool:
+        return self._local_net.is_down
+
+    @property
+    def hosts(self) -> frozendict[str, Layer3ConnectedWorkloadHost]:
+        hosts = dict(self._local_net)
+        hosts.update(self._cloud_hosts)
+        return frozendict(hosts)
+
+    @property
+    def address(self) -> IPv4Network:
+        return self._local_net.address
+
+    def tear_down(self) -> None:
+        return self._local_net.tear_down()
