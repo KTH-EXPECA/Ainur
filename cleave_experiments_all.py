1bf168d1010ab238153a6e4015272418d5be60ae
Manuel Olguín Muñoz <manuel@olguin.se>
2022-01-06 17:32:14 +0100

    tweaking stream

--- cleave_experiments_all.py
+++ cleave_experiments_all.py
@@ -564,7 +564,7 @@ if __name__ == '__main__':
 
     load_cfgs = [
         LoadConfig(
-            target_kbps=6000,
+            target_kbps=25000,
             packet_size_bytes=8000,
             client_hostname=c,
             server_hostname='elrond',
