02eef48738eb4bfe099aca704373025f9c53aa5a
Manuel Olguín Muñoz <manuel@olguin.se>
2022-01-20 23:21:52 +0100

    ec2 instance creation and teardown working

--- ainur/aws.py
+++ ainur/aws.py
@@ -1,11 +1,23 @@
 from __future__ import annotations
 
+import socket
+import time
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
-from typing import Any, Collection, Generator
+from dataclasses import dataclass
+from ipaddress import IPv4Address
+from typing import Collection, Generator, Tuple
 
 import boto3
+from loguru import logger
 from mypy_boto3_ec2.service_resource import Instance
+
+
+@dataclass(frozen=True, eq=True)
+class AWSHost:
+    name: str
+    ec2_instance_id: str
+    public_ip: IPv4Address
 
 
 @contextmanager
@@ -15,8 +27,12 @@ def aws_instance_ctx(
         instance_type: str = 't3.micro',
         key_name: str = 'ExPECA_AWS_Keys',
         security_groups: Collection[str] = ('sg-0170fa039ff0a56c4',),
-        region: str = 'eu-north-1'
-) -> Generator[Any, None, None]:
+        region: str = 'eu-north-1',
+        startup_timeout_s: int = 60 * 3
+) -> Generator[Tuple[AWSHost, ...], None, None]:
+    logger.warning(f'Deploying {num_instances} AWS compute instances of type '
+                   f'{instance_type}...')
+
     ec2 = boto3.resource('ec2', region_name=region)
     # noinspection PyTypeChecker
     instances = ec2.create_instances(
@@ -27,6 +43,7 @@ def aws_instance_ctx(
         KeyName=key_name,
         SecurityGroupIds=list(security_groups)
     )
+    logger.debug('Instances created.')
 
     # wait until instances are running and are accessible on port 22
     with ThreadPoolExecutor() as tpool:
@@ -34,9 +51,54 @@ def aws_instance_ctx(
             # first wait until instance is "running" (does not mean the OS is
             # fully initialized though)
             instance.wait_until_running()
+            instance.reload()
+            logger.debug(f'Instance {instance.instance_id} is running.')
 
             # now, attempt to open a socket connection to port 22 to verify
             # that the instance is ready
+            timeout = time.monotonic() + startup_timeout_s
+            while time.monotonic() <= timeout:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    try:
+                        sock.connect((instance.public_ip_address, 22))
+                        # connected, return
+                        logger.info(f'Instance {instance.instance_id} is '
+                                    f'ready.')
+                        return instance
+                    except (TimeoutError, ConnectionRefusedError):
+                        continue
+
+            # could not connect
+            raise TimeoutError(f'Timed-out waiting for '
+                               f'instance {instance.instance_id}.')
+
+        logger.debug('Waiting for instances to finish boot...')
+        instances = list(tpool.map(_wait_for_instance, instances))
+
+    # at this point, instances are up and running
+    yield tuple([AWSHost(
+        name='offload-1',
+        ec2_instance_id=inst.instance_id,
+        public_ip=IPv4Address(inst.public_ip_address)
+    ) for inst in instances])
+
+    # contextmanager shutting down, tear down and terminate all the instances
+    logger.warning('Shutting down AWS compute instances...')
+    with ThreadPoolExecutor() as tpool:
+        def _shutdown_instance(instance: Instance):
+            logger.debug(f'Terminating instance {instance.instance_id}...')
+            instance.terminate()
+            instance.wait_until_terminated()
+            logger.warning(f'Instance {instance.instance_id} terminated.')
+
+        # list() forces the .map() statement to be evaluated immediately
+        list(tpool.map(_shutdown_instance, instances))
 
 
+if __name__ == '__main__':
+    with aws_instance_ctx(10) as instances:
+        for i in instances:
+            print(i.public_ip)
+
+        input('Press any key to shut down.')
 
