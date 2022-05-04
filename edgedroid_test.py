d3e85810a26d217df6ca582816101ff47fab25b9
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-05-04 14:21:00 +0200

    wifi

--- edgedroid_test.py
+++ edgedroid_test.py
@@ -7,18 +7,18 @@ from ainur.swarm import *
 from ainur.swarm.storage import ExperimentStorage
 
 # used to indentify the correct virtual machine image to use for each AWS region
-with open('./offload-ami-ids.yaml', 'r') as fp:
+with open("./offload-ami-ids.yaml", "r") as fp:
     ami_ids = yaml.safe_load(fp)
-region = 'eu-north-1'
+region = "eu-north-1"
 # region = 'us-east-1'
 
 # the workload switch, no need to change this
 # should eventually go in a config file.
 switch = Switch(
-    name='glorfindel',
-    management_ip=IPv4Interface('192.168.0.2/16'),
-    username='cisco',
-    password='expeca',
+    name="glorfindel",
+    management_ip=IPv4Interface("192.168.0.2/16"),
+    username="cisco",
+    password="expeca",
 )
 
 # SDR access point configurations for this workload scenario
@@ -29,18 +29,19 @@ switch = Switch(
 # devices on the wire can talk to each other (and so devices connected by
 # wifi can reach the cloud! this is important).
 sdr_aps = [
-    # APSoftwareDefinedRadio(
-    #     name='RFSOM-00002',
-    #     management_ip=IPv4Interface('172.16.2.12/24'),
-    #     mac='02:05:f7:80:0b:19',
-    #     switch_port=42,
-    #     ssid='expeca_wlan_1',
-    #     net_name='eth_net',
-    #     channel=11,
-    #     beacon_interval=100,
-    #     ht_capable=True
-    # )
+    APSoftwareDefinedRadio(
+        name="RFSOM-00002",
+        management_ip=IPv4Interface("172.16.2.12/24"),
+        mac="02:05:f7:80:0b:19",
+        switch_port=42,
+        ssid="expeca_wlan_1",
+        net_name="eth_net",
+        channel=11,
+        beacon_interval=100,
+        ht_capable=True,
+    )
 ]
+
 
 # sdr STA configurations
 sdr_stas = [
@@ -68,41 +69,39 @@ sdr_stas = [
 # also note that if a device has more than one workload interface, ONLY ONE
 # WILL BE USED (and it will be selected arbitrarily!)
 hosts = {
-    'workload-client-00': LocalAinurHost(
-        management_ip=IPv4Interface('192.168.3.0/16'),
-        ansible_user='expeca',  # cloud instances have a different user
-        ethernets=frozendict({
-            'eth0': EthernetCfg(
-                ip_address=IPv4Interface('10.0.2.0/16'),
+    "workload-client-00": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.0/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        # ethernets=frozendict(
+        #     {
+        #         "eth0": EthernetCfg(
+        #             ip_address=IPv4Interface("10.0.2.0/16"),
+        #             routes=(
+        #                 IPRoute(
+        #                     to=IPv4Interface("172.16.1.0/24"),
+        #                     via=IPv4Address("10.0.1.0"),
+        #                 ),
+        #             ),
+        #             mac="dc:a6:32:b4:d8:b5",
+        #             wire_spec=WireSpec(net_name="eth_net", switch_port=25),
+        #         ),
+        #     }
+        # ),
+        # wifis=frozendict(),
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.0/16"),
                 routes=(
+                    # this route is necessary to reach the VPN to the cloud
                     IPRoute(
-                        to=IPv4Interface('172.16.1.0/24'),
-                        via=IPv4Address('10.0.1.0')
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
                     ),
                 ),
-                mac='dc:a6:32:b4:d8:b5',
-                wire_spec=WireSpec(
-                    net_name='eth_net',
-                    switch_port=25
-                )
-            ),
-        }),
-        wifis=frozendict(),
-        # ethernets=frozendict(),
-        # wifis=frozendict(
-        #     wlan1=WiFiCfg(
-        #         ip_address=IPv4Interface('10.0.2.0/16'),
-        #         routes=(
-        #             # this route is necessary to reach the VPN to the cloud
-        #             IPRoute(
-        #                 to=IPv4Interface('172.16.1.0/24'),
-        #                 via=IPv4Address('10.0.1.0')
-        #             ),
-        #         ),
-        #         mac='7c:10:c9:1c:3f:f0',
-        #         ssid='expeca_wlan_1'  # SDR wifi ssid
-        #     )
-        # )
+                mac="7c:10:c9:1c:3f:f0",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
     ),
     # 'workload-client-01': LocalAinurHost(
     #     management_ip=IPv4Interface('192.168.3.1/16'),
@@ -142,42 +141,46 @@ hosts = {
     # TODO: automatic way of configuring VPN gateway?
     # OLWE is the VPN host
     # it is only here to connect it to the network, dont deploy workloads on it.
-    'olwe'              : LocalAinurHost(
-        management_ip=IPv4Interface('192.168.0.4/16'),
-        ansible_user='expeca',
-        ethernets=frozendict({
-            'eth0': EthernetCfg(
-                ip_address=IPv4Interface('10.0.1.0/16'),
-                routes=(),
-                mac='dc:a6:32:bf:54:1b',
-                wire_spec=WireSpec(
-                    net_name='eth_net',
-                    switch_port=36,
-                )
-            )
-        }),
-        wifis=frozendict()
-    ),
-    'elrond'            : LocalAinurHost(
-        management_ip=IPv4Interface('192.168.1.2/16'),
-        ansible_user='expeca',
-        ethernets=frozendict({
-            'enp4s0': EthernetCfg(
-                ip_address=IPv4Interface('10.0.1.1/16'),
-                routes=(  # VPN route
-                    IPRoute(
-                        to=IPv4Interface('172.16.1.0/24'),
-                        via=IPv4Address('10.0.1.0')
+    # "olwe": LocalAinurHost(
+    #     management_ip=IPv4Interface("192.168.0.4/16"),
+    #     ansible_user="expeca",
+    #     ethernets=frozendict(
+    #         {
+    #             "eth0": EthernetCfg(
+    #                 ip_address=IPv4Interface("10.0.1.0/16"),
+    #                 routes=(),
+    #                 mac="dc:a6:32:bf:54:1b",
+    #                 wire_spec=WireSpec(
+    #                     net_name="eth_net",
+    #                     switch_port=36,
+    #                 ),
+    #             )
+    #         }
+    #     ),
+    #     wifis=frozendict(),
+    # ),
+    "elrond": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.1.2/16"),
+        ansible_user="expeca",
+        ethernets=frozendict(
+            {
+                "enp4s0": EthernetCfg(
+                    ip_address=IPv4Interface("10.0.1.1/16"),
+                    routes=(  # VPN route
+                        IPRoute(
+                            to=IPv4Interface("172.16.1.0/24"),
+                            via=IPv4Address("10.0.1.0"),
+                        ),
                     ),
-                ),
-                mac='d8:47:32:a3:25:20',
-                wire_spec=WireSpec(
-                    net_name='eth_net',
-                    switch_port=2,
+                    mac="d8:47:32:a3:25:20",
+                    wire_spec=WireSpec(
+                        net_name="eth_net",
+                        switch_port=2,
+                    ),
                 )
-            )
-        }),
-        wifis=frozendict()
+            }
+        ),
+        wifis=frozendict(),
     ),
 }
 
@@ -211,7 +214,7 @@ server_tag = "server"
 client_tag = "client"
 
 # language=yaml
-workload_def = f'''
+workload_def = f"""
 ---
 name: {workload_name}
 author: "Manuel Olguín Muñoz"
@@ -282,13 +285,14 @@ compose:
       depends_on:
       - server
 ...
-'''
+"""
 
 # noinspection DuplicatedCode
-if __name__ == '__main__':
-    ansible_ctx = AnsibleContext(base_dir=Path('ansible_env'))
-    workload: WorkloadSpecification = \
-        WorkloadSpecification.from_dict(yaml.safe_load(workload_def))
+if __name__ == "__main__":
+    ansible_ctx = AnsibleContext(base_dir=Path("ansible_env"))
+    workload: WorkloadSpecification = WorkloadSpecification.from_dict(
+        yaml.safe_load(workload_def)
+    )
 
     # prepare everything
     # if you dont want cloud instances, remove all CloudInstances and
@@ -322,10 +326,9 @@ if __name__ == '__main__':
 
         # start phy layer
         phy_layer: PhysicalLayer = stack.enter_context(
-            PhysicalLayer(hosts=hosts,
-                          radio_aps=sdr_aps,
-                          radio_stas=sdr_stas,
-                          switch=switch)
+            PhysicalLayer(
+                hosts=hosts, radio_aps=sdr_aps, radio_stas=sdr_stas, switch=switch
+            )
         )
 
         # init layer 3 connectivity
@@ -336,15 +339,15 @@ if __name__ == '__main__':
         #  configs into general host specification somehow??
         # swarm is a bit manual for now.
         swarm: DockerSwarm = stack.enter_context(swarm)
-        swarm.deploy_managers(hosts={hosts['elrond']: {}},
-                              location='edge',
-                              role='backend') \
-            .deploy_workers(
+        swarm.deploy_managers(
+            hosts={hosts["elrond"]: {}}, location="edge", role="backend"
+        ).deploy_workers(
             hosts={
-                hosts['workload-client-00']: {},
+                hosts["workload-client-00"]: {},
                 # hosts['workload-client-01']: {}
             },
-            role='client')
+            role="client",
+        )
 
         # start cloud instances
         # cloud.init_instances(len(cloud_hosts), ami_id=ami_ids[region])
@@ -364,17 +367,17 @@ if __name__ == '__main__':
             ExperimentStorage(
                 storage_name=workload.name,
                 storage_host=ManagedHost(
-                    management_ip=IPv4Interface('192.168.1.1/16'),
-                    ansible_user='expeca',
+                    management_ip=IPv4Interface("192.168.1.1/16"),
+                    ansible_user="expeca",
                 ),
                 network=ip_layer,
                 ansible_ctx=ansible_ctx,
-                ansible_quiet=False
+                ansible_quiet=False,
             )
         )
 
         swarm.deploy_workload(
             specification=workload,
             attach_volume=storage.docker_vol_name,
-            max_failed_health_checks=-1
+            max_failed_health_checks=-1,
         )
