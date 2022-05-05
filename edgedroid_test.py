6ff2d420f1067be1163f11e68f6910083d66c84e
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-05-05 15:06:45 +0200

    large scale test

--- edgedroid_test.py
+++ edgedroid_test.py
@@ -72,22 +72,6 @@ hosts = {
     "workload-client-00": LocalAinurHost(
         management_ip=IPv4Interface("192.168.3.0/16"),
         ansible_user="expeca",  # cloud instances have a different user
-        # ethernets=frozendict(
-        #     {
-        #         "eth0": EthernetCfg(
-        #             ip_address=IPv4Interface("10.0.2.0/16"),
-        #             routes=(
-        #                 IPRoute(
-        #                     to=IPv4Interface("172.16.1.0/24"),
-        #                     via=IPv4Address("10.0.1.0"),
-        #                 ),
-        #             ),
-        #             mac="dc:a6:32:b4:d8:b5",
-        #             wire_spec=WireSpec(net_name="eth_net", switch_port=25),
-        #         ),
-        #     }
-        # ),
-        # wifis=frozendict(),
         ethernets=frozendict(),
         wifis=frozendict(
             wlan1=WiFiCfg(
@@ -103,62 +87,169 @@ hosts = {
             )
         ),
     ),
-    # 'workload-client-01': LocalAinurHost(
-    #     management_ip=IPv4Interface('192.168.3.1/16'),
-    #     ansible_user='expeca',
-    #     # ethernets=frozendict({
-    #     #     'eth0': EthernetCfg(
-    #     #         ip_address=IPv4Interface('10.0.2.1/16'),
-    #     #         routes=(  # VPN route
-    #     #             IPRoute(
-    #     #                 to=IPv4Interface('172.16.1.0/24'),
-    #     #                 via=IPv4Address('10.0.1.0')
-    #     #             ),
-    #     #         ),
-    #     #         mac='dc:a6:32:bf:53:04',
-    #     #         wire_spec=WireSpec(
-    #     #             net_name='eth_net',
-    #     #             switch_port=26
-    #     #         )
-    #     #     ),
-    #     # }),
-    #     # wifis=frozendict(),
-    #     ethernets=frozendict(),
-    #     wifis=frozendict(
-    #         wlan1=WiFiCfg(
-    #             ip_address=IPv4Interface('10.0.2.1/16'),
-    #             routes=(
-    #                 IPRoute(
-    #                     to=IPv4Interface('172.16.1.0/24'),
-    #                     via=IPv4Address('10.0.1.0')
-    #                 ),
-    #             ),
-    #             mac='7c:10:c9:1c:3f:ea',
-    #             ssid='expeca_wlan_1'
-    #         )
-    #     )
-    # ),
-    # TODO: automatic way of configuring VPN gateway?
-    # OLWE is the VPN host
-    # it is only here to connect it to the network, dont deploy workloads on it.
-    # "olwe": LocalAinurHost(
-    #     management_ip=IPv4Interface("192.168.0.4/16"),
-    #     ansible_user="expeca",
-    #     ethernets=frozendict(
-    #         {
-    #             "eth0": EthernetCfg(
-    #                 ip_address=IPv4Interface("10.0.1.0/16"),
-    #                 routes=(),
-    #                 mac="dc:a6:32:bf:54:1b",
-    #                 wire_spec=WireSpec(
-    #                     net_name="eth_net",
-    #                     switch_port=36,
-    #                 ),
-    #             )
-    #         }
-    #     ),
-    #     wifis=frozendict(),
-    # ),
+    "workload-client-01": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.1/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.1/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="7c:10:c9:1c:3f:ea",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-02": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.2/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.2/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="7c:10:c9:1c:3f:e8",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-03": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.3/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.3/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="7c:10:c9:1c:3e:04",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    # client-04 is not working
+    "workload-client-05": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.5/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.5/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="7c:10:c9:1c:3e:a8",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-06": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.6/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.6/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="fc:34:97:25:a2:92",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-07": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.7/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.7/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="24:4b:fe:b7:26:92",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-08": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.8/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.8/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="dc:a6:32:bf:54:13",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-09": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.9/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.9/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="3c:7c:3f:a2:50:bd",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
+    "workload-client-10": LocalAinurHost(
+        management_ip=IPv4Interface("192.168.3.10/16"),
+        ansible_user="expeca",  # cloud instances have a different user
+        ethernets=frozendict(),
+        wifis=frozendict(
+            wlan1=WiFiCfg(
+                ip_address=IPv4Interface("10.0.2.10/16"),
+                routes=(
+                    # this route is necessary to reach the VPN to the cloud
+                    IPRoute(
+                        to=IPv4Interface("172.16.1.0/24"), via=IPv4Address("10.0.1.0")
+                    ),
+                ),
+                mac="fc:34:97:25:a2:0d",
+                ssid="expeca_wlan_1",  # SDR wifi ssid
+            )
+        ),
+    ),
     "elrond": LocalAinurHost(
         management_ip=IPv4Interface("192.168.1.2/16"),
         ansible_user="expeca",
@@ -205,13 +296,17 @@ cloud_hosts = [
     # ),
 ]
 
-task = "square00"
-model = "empirical"
-workload_name = "EdgeDroidTest"
+task = "test"
+model = "naive"
+workload_name = "EdgeDroidWiFi10"
 duration = "40m"
 image = "molguin/edgedroid2"
 server_tag = "server"
 client_tag = "client"
+
+task_slot = r".{{.Task.Slot}}"
+server_host = f"server{task_slot}"
+client_host = f"client{task_slot}"
 
 # language=yaml
 workload_def = f"""
@@ -227,19 +322,19 @@ compose:
   services:
     server:
       image: {image}:{server_tag}
-      hostname: server
+      hostname: "{server_host}"
+      environment:
+        EDGEDROID_SERVER_OUTPUT: "/opt/results/server{task_slot}_{task}_{model}.csv"
       command:
       - "--one-shot"
-      - "--output"
-      - "/opt/results/server_{task}_{model}.csv"
       - "--verbose"
       - "0.0.0.0"
       - "5000"
       - "{task}"
       deploy:
-        replicas: 1
+        replicas: 10
         placement:
-          max_replicas_per_node: 1  # 1 per cloud, 1 on elrond
+          max_replicas_per_node: 10
           constraints:
           - "node.labels.role==backend"
         restart_policy:
@@ -253,13 +348,17 @@ compose:
   
     client:
       image: {image}:{client_tag}
-      hostname: client
+      hostname: "{client_host}"
       volumes:
         - type: volume
           source: {workload_name}
           target: /opt/results/
           volume:
             nocopy: true
+      environment:
+        EDGEDROID_CLIENT_HOST: "{server_host}"
+        EDGEDROID_CLIENT_PORT: 5000
+        EDGEDROID_CLIENT_OUTPUT: "/opt/results/client{task_slot}_{task}_{model}.csv"
       command:
         - "-n"
         - "0.5"
@@ -269,13 +368,9 @@ compose:
         - "8"
         - "-m"
         - "{model}"
-        - "-o"
-        - "/opt/results/client_{task}_{model}.csv"
         - "--verbose"
-        - "server"
-        - "5000"
       deploy:
-        replicas: 1
+        replicas: 10
         placement:
           max_replicas_per_node: 1
           constraints:
