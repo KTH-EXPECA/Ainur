ed98af90a617e9dcfcae162eb6a7fb7d8a20edae
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-05-07 12:17:40 +0200

    fix PhysicalLayer side effect of clearing hosts dictionary

--- ainur/physical.py
+++ ainur/physical.py
@@ -5,8 +5,13 @@ from typing import Collection, Dict, Iterator, List, Mapping
 
 from loguru import logger
 
-from .hosts import APSoftwareDefinedRadio, LocalAinurHost, \
-    SoftwareDefinedRadio, StationSoftwareDefinedRadio, Switch
+from .hosts import (
+    APSoftwareDefinedRadio,
+    LocalAinurHost,
+    SoftwareDefinedRadio,
+    StationSoftwareDefinedRadio,
+    Switch,
+)
 from .managed_switch import ManagedSwitch
 from .sdr_manager import SDRManager, SDRManagerError
 
@@ -23,65 +28,66 @@ class PhysicalLayer(AbstractContextManager, Mapping[str, LocalAinurHost]):
     of physical layer connections.
     """
 
-    def __init__(self,
-                 hosts: Dict[str, LocalAinurHost],
-                 radio_aps: Collection[APSoftwareDefinedRadio],
-                 radio_stas: Collection[StationSoftwareDefinedRadio],
-                 switch: Switch):
+    def __init__(
+        self,
+        hosts: Dict[str, LocalAinurHost],
+        radio_aps: Collection[APSoftwareDefinedRadio],
+        radio_stas: Collection[StationSoftwareDefinedRadio],
+        switch: Switch,
+    ):
         """
         Parameters
         ----------
         """
 
         # check that radio names are unique in aps and stas
-        radio_names = set([r.name for r in radio_aps]) \
-            .union([r.name for r in radio_aps])
+        radio_names = set([r.name for r in radio_aps]).union(
+            [r.name for r in radio_aps]
+        )
 
         # TODO: need better way of checking radio uniqueness
 
         if len(radio_aps) + len(radio_stas) > len(radio_names):
-            raise PhyConfigError('Repeated radio ids in AP and STA '
-                                 'definitions!')
+            raise PhyConfigError("Repeated radio ids in AP and STA " "definitions!")
 
         radios: List[SoftwareDefinedRadio] = list(radio_aps)
         radios.extend(radio_stas)
 
-        logger.info('Setting up physical layer.')
+        logger.info("Setting up physical layer.")
         # Instantiate network's switch
-        self._switch = ManagedSwitch(name=switch.name,
-                                     credentials=(switch.username,
-                                                  switch.password),
-                                     address=switch.management_ip,
-                                     timeout=5)
+        self._switch = ManagedSwitch(
+            name=switch.name,
+            credentials=(switch.username, switch.password),
+            address=switch.management_ip,
+            timeout=5,
+        )
         try:
             # Make workload switch vlans
-            self._switch.make_connections(
-                hosts=hosts,
-                radios=radios
-            )
+            self._switch.make_connections(hosts=hosts, radios=radios)
 
             # Instantiate sdr network container
             try:
                 self._sdr_manager = SDRManager(
                     sdrs=radios,
-                    docker_base_url='unix://var/run/docker.sock',
-                    container_image_name='sdr_manager:latest',
-                    sdr_config_addr='/opt/sdr-manager',
+                    docker_base_url="unix://var/run/docker.sock",
+                    container_image_name="sdr_manager:latest",
+                    sdr_config_addr="/opt/sdr-manager",
                     use_jumbo_frames=False,
                 )
 
                 try:
                     # Make workload wireless LANS
-                    self._sdr_manager.create_wlans(hosts=hosts,
-                                                   sdr_aps=radio_aps,
-                                                   sdr_stas=radio_stas)
+                    self._sdr_manager.create_wlans(
+                        hosts=hosts, sdr_aps=radio_aps, sdr_stas=radio_stas
+                    )
                 except Exception:
                     self._sdr_manager.tear_down()
                     raise
 
             except SDRManagerError:
-                logger.warning('Skipping SDR initialization, no SDR networks '
-                               'specified.')
+                logger.warning(
+                    "Skipping SDR initialization, no SDR networks " "specified."
+                )
 
                 # this is to avoid null checks in tear down.
                 # TODO: maybe fix?
@@ -90,9 +96,11 @@ class PhysicalLayer(AbstractContextManager, Mapping[str, LocalAinurHost]):
                         pass
 
                 self._sdr_manager = DummySDRManager()
-            self._hosts = hosts
+            self._hosts = hosts.copy()  # make sure to copy so that teardown doesn't
+            # have the side effect of deleting the hosts dictionary used for the
+            # experiment!
 
-            logger.info('All connections are ready and double-checked.')
+            logger.info("All connections are ready and double-checked.")
         except Exception:
             self._switch.tear_down()
             raise
@@ -118,13 +126,13 @@ class PhysicalLayer(AbstractContextManager, Mapping[str, LocalAinurHost]):
         """
 
         # prepare a temp ansible environment and run the appropriate playbook
-        logger.warning('Tearing down physical layer!')
+        logger.warning("Tearing down physical layer!")
 
         self._switch.tear_down()
         self._sdr_manager.tear_down()
         self._hosts.clear()
 
-        logger.warning('Physical layer has been torn down.')
+        logger.warning("Physical layer has been torn down.")
 
     def __enter__(self) -> PhysicalLayer:
         return self
