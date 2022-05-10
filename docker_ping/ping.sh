7491a883042047a47afc165fa84d3fdeb5118e06
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-05-10 14:08:04 +0200

    make sure output hostname

--- docker_ping/ping.sh
+++ docker_ping/ping.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+hostname > "$OUTPUT"
 set -o pipefail
 if [ -z ${OUTPUT+x} ]; then
   ping "$HOST"
 else
-  ping "$HOST" 2>&1 | tee "$OUTPUT"
+  ping "$HOST" 2>&1 | tee -a "$OUTPUT"
 fi
