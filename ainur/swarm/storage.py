be69f9a7c386ad8dcbcb4cf801a2825aa9ba9fba
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2022-05-30 15:48:52 +0200

    fix so that volumes cannot be stuck forever

--- ainur/swarm/storage.py
+++ ainur/swarm/storage.py
@@ -29,19 +29,21 @@ class ExperimentStorage(AbstractContextManager):
     Brings up a Samba server for persistent Docker volumes for experiments.
     """
 
-    def __init__(self,
-                 storage_name: str,
-                 storage_host: ManagedHost,
-                 network: Layer3Network,
-                 ansible_ctx: AnsibleContext,
-                 host_path: PathLike = '/opt/expeca/experiments',
-                 daemon_port: int = 2375,
-                 ansible_quiet: bool = True):
+    def __init__(
+        self,
+        storage_name: str,
+        storage_host: ManagedHost,
+        network: Layer3Network,
+        ansible_ctx: AnsibleContext,
+        host_path: PathLike = "/opt/expeca/experiments",
+        daemon_port: int = 2375,
+        ansible_quiet: bool = True,
+    ):
         # TODO: documentation
         # TODO: needs to be further parameterized so it can be configured
         #  from a file.
 
-        logger.info(f'Initializing centralized storage {storage_name}.')
+        logger.info(f"Initializing centralized storage {storage_name}.")
         self._storage_name = storage_name
         self._vols: List[Tuple[AinurHost, Volume]] = list()
 
@@ -50,62 +52,54 @@ class ExperimentStorage(AbstractContextManager):
         exp_path = host_path / storage_name
 
         inventory = {
-            'all': {
-                'hosts': {
+            "all": {
+                "hosts": {
                     str(storage_host.management_ip.ip): {
-                        'ansible_host': str(storage_host.management_ip.ip),
-                        'ansible_user': storage_host.ansible_user,
+                        "ansible_host": str(storage_host.management_ip.ip),
+                        "ansible_user": storage_host.ansible_user,
                     }
                 }
             }
         }
 
-        logger.info(f'Creating paths on storage host {storage_host}.')
+        logger.info(f"Creating paths on storage host {storage_host}.")
         with ansible_ctx(inventory) as ansible_path:
             res = ansible_runner.run(
-                host_pattern='all',
-                module='ansible.builtin.file',
-                module_args=f'path={exp_path} state=directory',
+                host_pattern="all",
+                module="ansible.builtin.file",
+                module_args=f"path={exp_path} state=directory",
                 json_mode=False,
                 private_data_dir=str(ansible_path),
                 quiet=ansible_quiet,
-                cmdline='--become'
+                cmdline="--become",
             )
 
-            if res.status == 'failed':
+            if res.status == "failed":
                 raise ExperimentStorageException(
-                    'Could not create necessary paths '
-                    f'on storage host {storage_host}'
+                    "Could not create necessary paths "
+                    f"on storage host {storage_host}"
                 )
 
         # path exists, now set up smb server
-        logger.info(f'Deploying SMB/CIFS server on {storage_host}.')
+        logger.info(f"Deploying SMB/CIFS server on {storage_host}.")
         try:
             with docker_client_context(
-                    base_url=f'{storage_host.management_ip.ip}:{daemon_port}'
+                base_url=f"{storage_host.management_ip.ip}:{daemon_port}"
             ) as client:
                 self._container = client.containers.run(
-                    image='dperson/samba',  # FIXME: hardcoded things!
+                    image="dperson/samba",  # FIXME: hardcoded things!
                     remove=True,
-                    name='smb-server',
+                    name="smb-server",
                     command='-p -u "expeca;expeca" '
-                            '-s "expeca;/opt/expeca;no;no;no;expeca"',
+                    '-s "expeca;/opt/expeca;no;no;no;expeca"',
                     detach=True,
-                    ports={
-                        '139': '139',
-                        '445': '445'
-                    },
-                    volumes={
-                        f'{exp_path}': {
-                            'bind': '/opt/expeca',
-                            'mode': 'rw'
-                        }
-                    }
+                    ports={"139": "139", "445": "445"},
+                    volumes={f"{exp_path}": {"bind": "/opt/expeca", "mode": "rw"}},
                 )
                 # storage server is now running
         except Exception as e:
             raise ExperimentStorageException(
-                f'Could not deploy SMB/CIFS server on {storage_host}!'
+                f"Could not deploy SMB/CIFS server on {storage_host}!"
             ) from e
 
         try:
@@ -114,32 +108,29 @@ class ExperimentStorage(AbstractContextManager):
                 exceptions = deque()
                 ex_cond = threading.Condition()
 
-                def _add_storage(host: AinurHost) \
-                        -> Tuple[AinurHost, Volume]:
+                def _add_storage(host: AinurHost) -> Tuple[AinurHost, Volume]:
                     logger.info(
-                        f'Creating Docker volume for centralized storage '
-                        f'{storage_name} on host {host}.')
+                        f"Creating Docker volume for centralized storage "
+                        f"{storage_name} on host {host}."
+                    )
                     try:
                         with docker_client_context(
-                                base_url=f'{host.management_ip.ip}:'
-                                         f'{daemon_port}'
+                            base_url=f"{host.management_ip.ip}:" f"{daemon_port}"
                         ) as client:
                             host_ip = storage_host.management_ip.ip
                             return host, client.volumes.create(
-                                name=f'{storage_name}',
-                                driver='local',
-                                driver_opts=
-                                {
-                                    'type'  : 'cifs',
-                                    'device': f'//{host_ip}/expeca',
-                                    'o'     : f'addr={host_ip},'
-                                              f'username=expeca,password=expeca'
-                                }
+                                name=f"{storage_name}",
+                                driver="local",
+                                driver_opts={
+                                    "type": "cifs",
+                                    "device": f"//{host_ip}/expeca",
+                                    "o": f"addr={host_ip},"
+                                    f"username=expeca,password=expeca",
+                                },
                             )
                     except Exception as e:
                         logger.critical(
-                            'Exception when adding Docker volume on '
-                            f'host {host}.'
+                            "Exception when adding Docker volume on " f"host {host}."
                         )
                         logger.exception(e)
                         with ex_cond:
@@ -153,7 +144,7 @@ class ExperimentStorage(AbstractContextManager):
                     if len(exceptions) > 0:
                         raise exceptions.pop()
         except Exception as e:
-            logger.error('Could not create storage volumes.')
+            logger.error("Could not create storage volumes.")
             # in case of any exception, we need to bring down everything
             self.tear_down()
             raise e
@@ -168,8 +159,13 @@ class ExperimentStorage(AbstractContextManager):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.tear_down()
 
-    def tear_down(self) -> None:
-        logger.info('Tearing down centralized storage server.')
+    def tear_down(
+        self,
+        default_wait_seconds: float = 0.01,
+        max_wait_seconds: float = 5.0,
+        force_threshold_seconds: float = 5 * 60.0,
+    ) -> None:
+        logger.info("Tearing down centralized storage server.")
 
         with ThreadPoolExecutor() as tpool:
             exceptions = deque()
@@ -177,21 +173,47 @@ class ExperimentStorage(AbstractContextManager):
 
             def _remove_vol(host_vol: Tuple[AinurHost, Volume]):
                 host, vol = host_vol
-                wait = 0.01
+                wait = default_wait_seconds
+                ti = time.monotonic()
                 while True:
-                    try:
-                        vol.remove()
-                        logger.info(f'Successfully removed Docker volume '
-                                    f'{vol.name} from host {host}.')
-                        return
-                    except APIError as e:
-                        if e.status_code == 409:
-                            logger.warning(f'Volume {vol.name} on host {host} '
-                                           f'is busy, waiting to remove.')
-                            # duplicate waiting time on each try
-                            time.sleep(wait)
-                            wait *= 2
-                        else:
+                    if (time.monotonic() - ti) < force_threshold_seconds:
+                        # if we haven't reached the force threshold, try nicely
+                        try:
+                            vol.remove()
+                            logger.info(
+                                f"Successfully removed Docker volume "
+                                f"{vol.name} from host {host}."
+                            )
+                            return
+                        except APIError as e:
+                            if e.status_code == 409:
+                                logger.warning(
+                                    f"Volume {vol.name} on host {host} "
+                                    f"is busy, waiting to remove."
+                                )
+                                # duplicate waiting time on each try
+                                time.sleep(wait)
+                                wait = max(wait * 2, max_wait_seconds)
+                            else:
+                                logger.exception(e)
+                                with exc_cond:
+                                    exceptions.append(e)
+                                    exc_cond.notify_all()
+                                raise e
+                    else:
+                        # force
+                        try:
+                            logger.warning(
+                                f"Forcing removal of Docker volume "
+                                f"{vol.name} from host {host}."
+                            )
+                            vol.remove(force=True)
+                            return
+                        except APIError as e:
+                            logger.error(
+                                f"Could not remove Docker volume {vol.name} "
+                                f"from host {host}."
+                            )
                             logger.exception(e)
                             with exc_cond:
                                 exceptions.append(e)
